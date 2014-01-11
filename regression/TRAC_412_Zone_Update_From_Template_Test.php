6002a10e77fc20d5cd2190d0ec05a687eabada97
Edmondas Girkantas <eg@fbsd.lt>
2014-01-11 23:13:44 +0200

    Reformatted tests source code.

--- regression/TRAC_412_Zone_Update_From_Template_Test.php
+++ regression/TRAC_412_Zone_Update_From_Template_Test.php
@@ -5,52 +5,53 @@ require_once 'functional/common.php';
 
 class TRAC_446_Zone_Update_From_Template_Test extends PHPUnit_Extensions_SeleniumTestCase {
 
-	protected function setUp() {
-		$this->setBrowserUrl(BROWSER_URL);
-	}
+    protected function setUp() {
+        $this->setBrowserUrl(BROWSER_URL);
+    }
 
-	public function testZoneUpdateFromTemplate() {
-		Common::doLogin();
-		Common::doAddMasterZone('poweradmin.com');
+    public function testZoneUpdateFromTemplate() {
+        Common::doLogin();
+        Common::doAddMasterZone('poweradmin.com');
 
-		$this->clickAndWait("link=List zone templates");
-		$this->clickAndWait("link=Add zone template");
-		$this->type('templ_name', "www");
-		$this->type('templ_descr', "www");
-		$this->clickAndWait("commit");
+        $this->clickAndWait("link=List zone templates");
+        $this->clickAndWait("link=Add zone template");
+        $this->type('templ_name', "www");
+        $this->type('templ_descr', "www");
+        $this->clickAndWait("commit");
 
-		$this->clickAndWait("link=List zone templates");
+        $this->clickAndWait("link=List zone templates");
 
-		$this->clickAndWait("css=img[alt=\"[ Edit template ]\"]");
-		$this->clickAndWait("css=div.content > input.button");
-		$this->type('name', "www.[ZONE]");
-		$this->select('type', "label=CNAME");
-		$this->type('content', "[ZONE]");
-		$this->clickAndWait("commit");
+        $this->clickAndWait("css=img[alt=\"[ Edit template ]\"]");
+        $this->clickAndWait("css=div.content > input.button");
+        $this->type('name', "www.[ZONE]");
+        $this->select('type', "label=CNAME");
+        $this->type('content', "[ZONE]");
+        $this->clickAndWait("commit");
 
-		$this->clickAndWait("link=List zones");
-		$this->clickAndWait("css=img[alt=\"[ View zone poweradmin.com ]\"]");
-		$this->select('zone_template', 'label=www');
-		$this->clickAndWait("template_change");
+        $this->clickAndWait("link=List zones");
+        $this->clickAndWait("css=img[alt=\"[ View zone poweradmin.com ]\"]");
+        $this->select('zone_template', 'label=www');
+        $this->clickAndWait("template_change");
 
-		$this->clickAndWait("link=List zones");
-		$this->clickAndWait("css=img[alt=\"[ View zone poweradmin.com ]\"]");
+        $this->clickAndWait("link=List zones");
+        $this->clickAndWait("css=img[alt=\"[ View zone poweradmin.com ]\"]");
 //		$this->verifyValue("record[1][name]", "www.poweradmin.com");
         $this->verifyValue("//input[contains(@name,'name')]", "www.poweradmin.com");
 //		$this->verifyValue("record[1][content]", "poweradmin.com");
         $this->verifyValue("//input[contains(@name,'content')]", "poweradmin.com");
 
-		$this->clickAndWait("link=List zone templates");
-		$this->clickAndWait("css=img[alt=\"[ Edit template ]\"]");
-		$this->clickAndWait("css=img[alt=\"[ Delete record ]\"]");
-		$this->clickAndWait("css=input.button");
+        $this->clickAndWait("link=List zone templates");
+        $this->clickAndWait("css=img[alt=\"[ Edit template ]\"]");
+        $this->clickAndWait("css=img[alt=\"[ Delete record ]\"]");
+        $this->clickAndWait("css=input.button");
 
-		$this->clickAndWait("link=List zone templates");
-		$this->clickAndWait("css=img[alt=\"[ Delete template ]\"]");
-		$this->clickAndWait("css=input.button");
+        $this->clickAndWait("link=List zone templates");
+        $this->clickAndWait("css=img[alt=\"[ Delete template ]\"]");
+        $this->clickAndWait("css=input.button");
 
-		Common::doRemoveZone('poweradmin.com');
-	}
+        Common::doRemoveZone('poweradmin.com');
+    }
+
 }
 
 ?>
--- regression/TRAC_446_Quote_Escape_Test.php
+++ regression/TRAC_446_Quote_Escape_Test.php
@@ -5,30 +5,31 @@ require_once 'functional/common.php';
 
 class TRAC_446_Quote_Escape_Test extends PHPUnit_Extensions_SeleniumTestCase {
 
-	protected function setUp() {
-		$this->setBrowserUrl(BROWSER_URL);
-	}
+    protected function setUp() {
+        $this->setBrowserUrl(BROWSER_URL);
+    }
 
-	public function testQuoteEscape() {
+    public function testQuoteEscape() {
         $this->markTestSkipped("Test case fails, need to review it");
 
-		Common::doLogin();
-		Common::doAddMasterZone('poweradmin.com');
+        Common::doLogin();
+        Common::doAddMasterZone('poweradmin.com');
 
-		$this->clickAndWait("link=List zones");
-		$this->clickAndWait("css=img[alt=\"[ View zone poweradmin.com ]\"]");
-		$this->select('type', 'label=TXT');
-		$this->type('content', "var='value'");
-		$this->clickAndWait("//input[@name='commit' and @value='Add record']");
+        $this->clickAndWait("link=List zones");
+        $this->clickAndWait("css=img[alt=\"[ View zone poweradmin.com ]\"]");
+        $this->select('type', 'label=TXT');
+        $this->type('content', "var='value'");
+        $this->clickAndWait("//input[@name='commit' and @value='Add record']");
 
-		$this->clickAndWait("link=List zones");
-		$this->clickAndWait("css=img[alt=\"[ View zone poweradmin.com ]\"]");
+        $this->clickAndWait("link=List zones");
+        $this->clickAndWait("css=img[alt=\"[ View zone poweradmin.com ]\"]");
 //		$this->verifyValue("record[2][content]", "var='value'");
         //TODO: this check fails
         $this->verifyValue("//input[contains(@name,'content')]", "var='value'");
 
-		Common::doRemoveZone('poweradmin.com');
-	}
+        Common::doRemoveZone('poweradmin.com');
+    }
+
 }
 
 ?>

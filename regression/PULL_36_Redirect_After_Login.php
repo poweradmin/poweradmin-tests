1e38105edf0488e37d98f70ecd3e634e1677c43d
Edmondas Girkantas <eg@fbsd.lt>
2014-01-14 20:37:30 +0200

    Improved test to check if user is redirected to requested page after logon

--- regression/PULL_36_Redirect_After_Login.php
+++ regression/PULL_36_Redirect_After_Login.php
@@ -8,13 +8,17 @@ class PULL_36_Redirect_After_Login extends PHPUnit_Extensions_SeleniumTestCase {
     protected function setUp() {
         $this->setBrowserUrl(BROWSER_URL);
     }
-    
+
     // https://github.com/internetjanitor/poweradmin/commit/d246af7425bb4b1cb3c2fbad7712cdd5e587214e
-    // https://github.com/internetjanitor/poweradmin/commit/96b217beb03d8313e0d34d2ae1cc9a2a25299232
     public function testRedirect() {
-        Common::doLogin('list_zones.php');
+        Common::doLogin('admin', 'list_zones.php');
         $this->verifyTextPresent('There are no zones to show in this listing.');
-        
+    }
+
+    // https://github.com/internetjanitor/poweradmin/commit/96b217beb03d8313e0d34d2ae1cc9a2a25299232
+    public function testRedirectWithParams() {
+        Common::doLogin('admin', 'edit_user.php?id=1');
+        $this->verifyTextPresent('Edit user "Administrator"');
     }
 
 }
